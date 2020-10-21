dd96eaa42c9bc8ecd637bf7ca6344cb018495aaa
Tawnee Harris <tawneeh@icloud.com>
2020-10-21 11:32:00 -0700

    initial calculator2 html and js
    Co-authored-by: Riley McAdoo <riley.mcadoo@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+const add = function(number1, number2) {
+  return number1 + number2;
+};
+
+const subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+const multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+const divide = function(number1, number2) {
+  return number1 / number2;
+};
